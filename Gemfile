2dfcb15592e108a05b4e924c124768ec94be7f45
Abraham Heward <aheward@rsmart.com>
2013-01-31 13:15:23 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "rspec"
+gem "cucumber"
+gem "test-factory", ">=0.0.7"
+gem "watir-webdriver", ">=0.6.1"
---
+++ lib/kuality-coeus.rb
@@ -0,0 +1,29 @@
+require 'test-factory'
+Dir["#{File.dirname(__FILE__)}/kuality-coeus/*.rb"].each {|f| require f }
+Dir["#{File.dirname(__FILE__)}/kuality-coeus/page_objects/*.rb"].each {|f| require f }
+Dir["#{File.dirname(__FILE__)}/kuality-coeus/data_objects/*.rb"].each {|f| require f }
+
+# Initialize this class at the start of your test cases to
+# open the specified test browser at the specified Sakai welcome page URL.
+#
+# The initialization will
+# create the @browser variable used throughout the page classes
+class Kuality
+
+  attr_reader :browser
+
+  def initialize(web_browser)
+    if web_browser == :saucelabs
+      @browser = Watir::Browser.new(
+          :remote,
+          :url => "http://#{ENV['username']}:#{ENV['api_key']}@ondemand.saucelabs.com:80/wd/hub",
+          :desired_capabilities => $environment
+      )
+    else
+      @browser = Watir::Browser.new web_browser
+      @browser.window.resize_to(1400,900)
+    end
+    @browser.goto $base_url
+  end
+
+end
