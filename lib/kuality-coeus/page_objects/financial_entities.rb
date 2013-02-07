114c9e873fa241e8fba2f00e6fd809604d85d7e4
Abraham Heward <aheward@rsmart.com>
2013-02-07 12:55:46 -0700

    More changes related to the new test site.

--- lib/kuality-coeus/page_objects/financial_entities.rb
+++ lib/kuality-coeus/page_objects/financial_entities.rb
@@ -1,14 +1,13 @@
 class FinancialEntities < BasePage
-  
-  header_tabs
+
   global_buttons
 
   class << self
 
     def financial_entities_tabs
-      action(:reporter) { |b| b.button(value: "Reporter").click }
-      action(:new_financial_entity) { |b| b.button(value: "New Financial Entity").click }
-      action(:my_financial_entities) { |b| b.button(value: "My Financial Entities").click }
+      button "Reporter"
+      button "New Financial Entity"
+      button "My Financial Entities"
     end
 
   end
