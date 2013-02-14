c0d06bf943921f2e3e3a499827da75188e5aa975
Abraham Heward <aheward@rsmart.com>
2013-02-14 09:47:04 -0700

    Added the Budget folder and moved the budget versions file into it.

--- features/step_definitions/creating_a_proposal.rb
+++ features/step_definitions/creating_a_proposal.rb
@@ -4,10 +4,7 @@ Given /^I am logged in as admin$/ do
 end
 When /^I create a proposal$/ do
   @proposal = create ProposalDevelopmentObject
-  #@goo = create PermissionsObject, document_id: "4255"
-  p @proposal.permissions.roles
 end
 Then /^It's created$/ do
-  sleep 15
   on(Proposal).feedback.should=='Document was successfully saved.'
 end
