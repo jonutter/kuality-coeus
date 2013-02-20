f9312696efb3d80c12b1c15b53a4395c5c1bb3f2
Abraham Heward <aheward@rsmart.com>
2013-02-20 10:05:08 -0700

    created the .errors method in the proposal class that collects all the errors on the various tabs.

--- lib/kuality-coeus/page_objects/proposal_development/proposal.rb
+++ lib/kuality-coeus/page_objects/proposal_development/proposal.rb
@@ -44,6 +44,12 @@ class Proposal < ProposalDevelopmentDocument
   element(:required_fields_errors) { |b| b.frm.div(index: 1, class: "left-errmsg-tab").div(index: 0).divs(style: "display:list-item;margin-left:20px;") }
 
   # An array whose elements are the text contents of all errors that appear in the tabs.
+  # It will NOT contain the error message at the top of the page, or the error that appears when
+  # a proposal is deleted.
   def errors
+    errs = []
+    overview_tab_errors.each { |err| errs << err.text }
+    required_fields_errors.each { |err| errs << err.text }
+  end
 
 end
