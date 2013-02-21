d5b33a68ee3498c1cfda9a6593c3048b25ddcb00
Abraham Heward <aheward@rsmart.com>
2013-02-21 12:52:17 -0700

    Starting building the structures for the Committee Document stuff. Nothing too involved, yet, as I'm not sure what's going to be important at this point.

--- lib/kuality-coeus/data_objects/committee_document.rb
+++ lib/kuality-coeus/data_objects/committee_document.rb
@@ -8,7 +8,7 @@ class CommitteeDocumentObject
   attr_accessor :description, :committee_id, :document_id, :status, :committee_name,
                 :home_unit, :min_members_for_quorum, :maximum_protocols,
                 :adv_submission_days, :review_type, :last_updated, :updated_user,
-                :initiator
+                :initiator, :members, :areas_of_research, :type
   
   def initialize(browser, opts={})
     @browser = browser
@@ -21,7 +21,10 @@ class CommitteeDocumentObject
       min_members_for_quorum: rand(100).to_s,
       maximum_protocols: rand(100).to_s,
       adv_submission_days: rand(365).to_s,
-      review_type: 'Full'
+      review_type: 'Full',
+      members: CommitteeMemberCollection.new,
+      areas_of_research: [],
+      schedule: CommitteeScheduleCollection.new
     }
     set_options(defaults.merge(opts))
   end
@@ -36,30 +39,44 @@ class CommitteeDocumentObject
       comm.description.set @description
       comm.committee_id.set @committee_id
       comm.committee_name.set @committee_name
+      @type=comm.type.pick @type
       comm.home_unit.set @home_unit
       comm.min_members_for_quorum.set @min_members_for_quorum
       comm.maximum_protocols.set @maximum_protocols
-      comm.adv_submission_days
+      comm.adv_submission_days.set @adv_submission_days
+      @review_type=comm.review_type.pick @review_type
       comm.save
     end
   end
-    
-  def edit opts={}
-    
-    set_options(opts)
-  end
 
   def submit
-    # TODO: Add conditional navigation here
-    # Currently this method assumes you're already
-    # on the relevant Committee document page.
+    navigate
     on(Committee).submit
   end
 
-  def view
-    
+  # =======
+  private
+  # =======
+
+  # Nav Aids...
+
+  def navigate
+    open_document unless on_document?
+    on(Committee).committee unless on_page?
   end
-  
+
+  def on_page?
+    # Note, the rescue clause should be
+    # removed when the Selenium bug with
+    # firefox elements gets fixed. This is
+    # still broken in selenium-webdriver 2.29
+    begin
+      on(Committee).committee_id_field.exist?
+    rescue
+      false
+    end
+  end
+
 end
     
       
