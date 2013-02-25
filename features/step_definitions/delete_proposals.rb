0ad13af398d73e237a5e5b913a94d9842c828dd1
Abraham Heward <aheward@rsmart.com>
2013-02-25 07:14:22 -0700

    Added the view method to the proposal development object.

--- features/step_definitions/delete_proposals.rb
+++ features/step_definitions/delete_proposals.rb
@@ -1,8 +1,9 @@
 When /^I delete the proposal$/ do
   @proposal.delete
 end
+
 Then /^The proposal is deleted$/ do
   @proposal.status.should == 'CANCELED'
-  @proposal.view #TODO Need this method
+  @proposal.view
   on(Proposal).error_message.should == 'The Development Proposal has been deleted.'
 end
