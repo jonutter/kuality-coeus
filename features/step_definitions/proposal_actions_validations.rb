50ad84f15432200f945e873e4bf10215286b2d06
Jonathan Utter <jutter@rsmart.com>
2013-02-26 09:46:05 -0700

    updating steps in proposal actions validations rb

--- features/step_definitions/proposal_actions_validations.rb
+++ features/step_definitions/proposal_actions_validations.rb
@@ -1,7 +1,9 @@
 And /^I activate a validation check$/ do
   on(Proposal).proposal_actions
+  on(ProposalActions).show_data_validation
   on(ProposalActions).turn_on_validation
 end
 Then /^I should see a key personal information error$/ do
-  pending
+  on(ProposalActions).show_key_personnel_errors
+
 end
