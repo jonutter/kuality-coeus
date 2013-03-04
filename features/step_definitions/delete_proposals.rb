498f53d379dcb10d5e095bed15e620c08201e9e7
Abraham Heward <aheward@rsmart.com>
2013-03-04 11:55:42 -0700

    Removed a TODO because the method was created.

--- features/step_definitions/delete_proposals.rb
+++ features/step_definitions/delete_proposals.rb
@@ -3,6 +3,6 @@ When /^I delete the proposal$/ do
 end
 Then /^The proposal is deleted$/ do
   @proposal.status.should == 'CANCELED'
-  @proposal.view #TODO Need this method
+  @proposal.view
   on(Proposal).error_message.should == 'The Development Proposal has been deleted.'
 end
