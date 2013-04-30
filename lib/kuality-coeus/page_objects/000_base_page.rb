7648cb3c9ae6715328451292d1f00152a444f533
Abraham Heward <aheward@rsmart.com>
2013-04-30 15:17:04 -0700

    Fix to the results return method so that it can't accidentally pick the header row.

--- lib/kuality-coeus/page_objects/000_base_page.rb
+++ lib/kuality-coeus/page_objects/000_base_page.rb
@@ -49,7 +49,8 @@ class BasePage < PageFactory
     def search_results_table
       element(:results_table) { |b| b.frm.table(id: 'row') }
       action(:return_value) { |match, p| p.results_table.row(text: /#{match}/).link(text: 'return value').click }
-      action(:return_random) { |b| b.results_table.row(index: rand(b.results_table.rows.length)).link(text: 'return value').click }
+      action(:return_random) { |b| b.return_value_links[rand(b.return_value_links.length)].click }
+      element(:return_value_links) { |b| b.results_table.links(text: 'return value') }
     end
 
     def budget_header_elements
