6879cb5c52503ec4aeb151b4ee960b020fad15f4
Abraham Heward <aheward@rsmart.com>
2013-05-23 08:29:10 -0700

    Merge from Jon Utter.

--- lib/kuality-coeus/page_objects/action_list_filter.rb
+++ lib/kuality-coeus/page_objects/action_list_filter.rb
@@ -3,5 +3,6 @@ class ActionListFilter < BasePage
   global_buttons
 
   element(:document_title) { |b| b.frm.text_field(name: 'filter.documentTitle') }
-  action(:filter) { |b| b.frm.button(name: 'methodToCall.filter').click b.loading }
+  action(:filter) { |b| b.frm.button(name: 'methodToCall.filter').click; b.loading }
+
 end
