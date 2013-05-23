60e06262ca5e038ec696f3de2fbe4909e405a8d2
Abraham Heward <aheward@rsmart.com>
2013-05-23 10:04:08 -0700

    Added element defs for the Rates page in Budget Versions.

--- lib/kuality-coeus/page_objects/budget/rates.rb
+++ lib/kuality-coeus/page_objects/budget/rates.rb
@@ -2,4 +2,14 @@ class Rates < BudgetDocument
 
   glbl 'Sync All Rates', 'Reset All Rates'
 
+  # ========
+  private
+  # ========
+  
+  element(:research_fa_table) { |b| b.frm.table(id: 'Research F & A') }
+  element(:fringe_benefits_table) { |b| b.frm.table(id: 'Fringe Benefits') }
+  element(:inflation_table) { |b| b.frm.table(id: 'Inflation') }
+  element(:vacation_table) { |b| b.frm.table(id: 'Vacation') }
+  element(:other_table) { |b| b.frm.table(id: 'Other') }
+  
 end
