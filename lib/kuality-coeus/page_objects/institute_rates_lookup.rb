232eeb137d694e2f923b81a799a553bfa57e90cb
Abraham Heward <aheward@rsmart.com>
2013-05-24 11:28:07 -0700

    addition of institute rates lookup page class.

---
+++ lib/kuality-coeus/page_objects/institute_rates_lookup.rb
@@ -0,0 +1,11 @@
+class InstituteRatesLookup < BasePage
+
+  tiny_buttons
+  search_results_table
+
+  element(:activity_type_code) { |b| b.frm.text_field(name: 'activityTypeCode') }
+  element(:fiscal_year) { |b| b.frm.text_field(name: 'fiscalYear') }
+  action(:on_off_campus) { |value, b| b.frm.radio(name: 'onOffCampusFlag', value: value).set }
+  element(:rate_class_code) { |b| b.frm.text_field(name: 'rateClassCode') }
+
+end
