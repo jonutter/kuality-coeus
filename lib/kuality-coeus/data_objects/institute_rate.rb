0995c10bcd4e2f6f27c5cf8fc5aa5d7e7b0a5c0f
Abraham Heward <aheward@rsmart.com>
2013-05-29 10:00:30 -0700

    Creation of the institute rate data object and a couple associated methods.

--- lib/kuality-coeus/data_objects/institute_rate.rb
+++ lib/kuality-coeus/data_objects/institute_rate.rb
@@ -3,7 +3,7 @@ class InstituteRateObject
   include Foundry
   include DataFactory
   include StringFactory
-  include DataFactory
+  include DateFactory
   include Navigation
 
   ACTIVITY_TYPES = {
@@ -44,63 +44,63 @@ class InstituteRateObject
       'Other'                             => '9'
   }
 
-  RATE_TYPES = [
-    {class: '1', type: '1', description: 'MTDC'},
-    {class: '10', type: '1', description: 'Lab Allocation - Salaries'},
-    {class: '11', type: '1', description: 'Lab Allocation - M&S'},
-    {class: '12', type: '1', description: 'Lab Allocation - Utilities'},
-    {class: '13', type: '1', description: 'MTDC - Award'},
-    {class: '13', type: '10', description: 'FUNSN'},
-    {class: '13', type: '11', description: 'FUNNX'},
-    {class: '13', type: '12', description: 'FUNSNX'},
-    {class: '13', type: '13', description: 'FUNSAX'},
-    {class: '13', type: '14', description: 'FUNNXF'},
-    {class: '13', type: '15', description: 'FUNMNX'},
-    {class: '13', type: '16', description: 'FUNMFX'},
-    {class: '13', type: '17', description: 'SLNSN'},
-    {class: '13', type: '18', description: 'SLNMN'},
-    {class: '13', type: '19', description: 'SLNMF'},
-    {class: '13', type: '2', description: 'TDC - DO NOT USE'},
-    {class: '13', type: '20', description: 'GENSN'},
-    {class: '13', type: '21', description: 'GENSNF'},
-    {class: '13', type: '22', description: 'RESSN'},
-    {class: '13', type: '23', description: 'FUNSAN'},
-    {class: '13', type: '24', description: 'FUNFN'},
-    {class: '13', type: '25', description: 'RESTDC'},
-    {class: '13', type: '26', description: 'FUNTDC'},
-    {class: '13', type: '27', description: 'RESTG'},
-    {class: '13', type: '28', description: 'RESTDE'},
-    {class: '13', type: '29', description: 'BIMN'},
-    {class: '13', type: '3', description: 'Other'},
-    {class: '13', type: '4', description: 'None'},
-    {class: '13', type: '5', description: 'RESMN'},
-    {class: '13', type: '6', description: 'RESMF'},
-    {class: '13', type: '7', description: 'EXCLU'},
-    {class: '13', type: '8', description: 'RESEB'},
-    {class: '13', type: '9', description: 'RESMFF'},
-    {class: '2', type: '1', description: 'TDC'},
-    {class: '3', type: '1', description: 'S&W'},
-    {class: '4', type: '1', description: 'Salaries'},
-    {class: '4', type: '2', description: 'Materials and Services'},
-    {class: '5', type: '1', description: 'Research Rate'},
-    {class: '5', type: '2', description: 'UROP Rate'},
-    {class: '5', type: '3', description: 'EB on LA'},
-    {class: '5', type: '4', description: 'another EB rate'},
-    {class: '5', type: '5', description: 'eb added in pb'},
-    {class: '5', type: '6', description: 'Award Special EB Rate'},
-    {class: '7', type: '1', description: 'Faculty Salaries (6/1)'},
-    {class: '7', type: '2', description: 'Administrative Salaries (7/1)'},
-    {class: '7', type: '3', description: 'Support Staff Salaries (4/1)'},
-    {class: '7', type: '4', description: 'Materials and Services'},
-    {class: '7', type: '5', description: 'Research Staff (1/1)'},
-    {class: '7', type: '6', description: 'Students (6/1)'},
-    {class: '8', type: '1', description: 'Vacation'},
-    {class: '8', type: '2', description: 'Vacation on LA'},
-    {class: '9', type: '1', description: 'Other'}
-  ]
+  RATE_TYPES = {
+      'MTDC'=>{class: '1', type: '1'},
+      'Lab Allocation - Salaries'=>{class: '10', type: '1'},
+      'Lab Allocation - M&S'=>{class: '11', type: '1'},
+      'Lab Allocation - Utilities'=>{class: '12', type: '1'},
+      'MTDC - Award'=>{class: '13', type: '1'},
+      'FUNSN'=>{class: '13', type: '10'},
+      'FUNNX'=>{class: '13', type: '11'},
+      'FUNSNX'=>{class: '13', type: '12'},
+      'FUNSAX'=>{class: '13', type: '13'},
+      'FUNNXF'=>{class: '13', type: '14'},
+      'FUNMNX'=>{class: '13', type: '15'},
+      'FUNMFX'=>{class: '13', type: '16'},
+      'SLNSN'=>{class: '13', type: '17'},
+      'SLNMN'=>{class: '13', type: '18'},
+      'SLNMF'=>{class: '13', type: '19'},
+      'TDC - DO NOT USE'=>{class: '13', type: '2'},
+      'GENSN'=>{class: '13', type: '20'},
+      'GENSNF'=>{class: '13', type: '21'},
+      'RESSN'=>{class: '13', type: '22'},
+      'FUNSAN'=>{class: '13', type: '23'},
+      'FUNFN'=>{class: '13', type: '24'},
+      'RESTDC'=>{class: '13', type: '25'},
+      'FUNTDC'=>{class: '13', type: '26'},
+      'RESTG'=>{class: '13', type: '27'},
+      'RESTDE'=>{class: '13', type: '28'},
+      'BIMN'=>{class: '13', type: '29'},
+      'Other'=>{class: '13', type: '3'},
+      'None'=>{class: '13', type: '4'},
+      'RESMN'=>{class: '13', type: '5'},
+      'RESMF'=>{class: '13', type: '6'},
+      'EXCLU'=>{class: '13', type: '7'},
+      'RESEB'=>{class: '13', type: '8'},
+      'RESMFF'=>{class: '13', type: '9'},
+      'TDC'=>{class: '2', type: '1'},
+      'S&W'=>{class: '3', type: '1'},
+      'Salaries'=>{class: '4', type: '1'},
+      'Materials and Services'=>{class: '4', type: '2'},
+      'Research Rate'=>{class: '5', type: '1'},
+      'UROP Rate'=>{class: '5', type: '2'},
+      'EB on LA'=>{class: '5', type: '3'},
+      'another EB rate'=>{class: '5', type: '4'},
+      'eb added in pb'=>{class: '5', type: '5'},
+      'Award Special EB Rate'=>{class: '5', type: '6'},
+      'Faculty Salaries (6/1)'=>{class: '7', type: '1'},
+      'Administrative Salaries (7/1)'=>{class: '7', type: '2'},
+      'Support Staff Salaries (4/1)'=>{class: '7', type: '3'},
+      'Materials and Services'=>{class: '7', type: '4'},
+      'Research Staff (1/1)'=>{class: '7', type: '5'},
+      'Students (6/1)'=>{class: '7', type: '6'},
+      'Vacation'=>{class: '8', type: '1'},
+      'Vacation on LA'=>{class: '8', type: '2'},
+      'Other'=>{class: '9', type: '1'}
+  }
 
-  attr_accessor :activity_type_code, :fiscal_year, :on_off_campus_flag,
-                :rate_class_code, :rate_type_code, :start_date, :unit_number,
+  attr_accessor :activity_type, :activity_type_code, :fiscal_year, :on_off_campus_flag,
+                :rate_type, :rate_class_code, :rate_type_code, :start_date, :unit_number,
                 :rate, :active, :description
 
   def initialize(browser, opts={})
@@ -109,12 +109,19 @@ class InstituteRateObject
     defaults = {
       description: random_alphanums,
       fiscal_year: right_now[:year],
-      activity_type_code: '1',
+      activity_type: 'Instruction',
       on_off_campus_flag: :set,
-
+      rate_type: 'Salaries',
+      start_date: "01/01/#{right_now[:year]}",
+      unit_number: '000001',
+      rate: '4.5',
+      active: :set
     }
 
     set_options(defaults.merge(opts))
+    @activity_type_code = ACTIVITY_TYPES[@activity_type]
+    @rate_class_code = RATE_TYPES[@rate_type][:class]
+    @rate_type_code = RATE_TYPES[@rate_type][:type]
   end
 
   def create
@@ -128,15 +135,49 @@ class InstituteRateObject
     end
   end
 
+  def exist?
+    navigate
+    search
+    if on(InstituteRatesLookup).results_table.present?
+      return true
+    else
+      return false
+    end
+  end
+
+  def delete
+    navigate
+    search
+    on(InstituteRatesLookup).delete_item "#{@rate_type}"
+    on InstituteRatesMaintenance do |del|
+      del.description.set @description
+      del.blanket_approve
+    end
+  end
+
+  def search
+    on InstituteRatesLookup do |look|
+      fill_out look, :activity_type_code, :fiscal_year, :rate_class_code,
+               :rate_type_code, :unit_number, :rate
+      look.on_off_campus campus_lookup[@on_off_campus_flag]
+      look.search
+    end
+  end
+
   # =========
   private
   # =========
 
   def navigate
+    # TODO: Throw some conditional logic in here so that we don't navigate every time.
     visit Maintenance do |page|
       page.close_parents
       page.institute_rate
     end
+  end
+
+  def campus_lookup
+    { :set=>'Y', :clear=>'N' }
   end
 
 end
