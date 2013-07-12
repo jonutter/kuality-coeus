eb1c2e699e181082e7549587025b95eaf74a1642
Abraham Heward <aheward@rsmart.com>
2013-07-12 08:03:35 -0700

    Added a stepdef for grants.gov PI users.

--- features/step_definitions/users.rb
+++ features/step_definitions/users.rb
@@ -36,6 +36,9 @@ end
 # them if they don't exist in the system.
 Given /^I have a user with the system role: '(.*)'$/ do |role|
   user = make_role role
+  # TODO: Need to make this more robust--because what happens if
+  # You're logged in with a user who doesn't have rights to
+  # create new users?
   user.create unless user.exists?
 end
 
@@ -48,4 +51,10 @@ Given /^I have users with the following roles: (.*)$/ do |roles|
     user = make_role r
     user.create unless user.exists?
   end
+end
+
+Given /^I have a user that can be a PI for Grants.gov proposals$/ do
+  # TODO: Make this more robust when we really know what it takes
+  # to be a grants.gov PI...
+  @grants_gov_pi = make_user 'grantsgov'
 end
