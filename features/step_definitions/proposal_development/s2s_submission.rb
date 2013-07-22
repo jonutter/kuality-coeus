819d7a9894471252c594d2a3aa6fb45f434dc096
Jonathan Utter <jutter@rsmart.com>
2013-07-22 11:10:03 -0700

    Altered the submit method in Proposal_development.rb for :to_s2s

--- features/step_definitions/proposal_development/s2s_submission.rb
+++ features/step_definitions/proposal_development/s2s_submission.rb
@@ -1,10 +1,10 @@
 Then /^Submission details will be immediately available on the S2S tab$/ do
    on S2S do |page|
      page.expand_all
-     page.submission_details.should exist
+     page.submission_details_table.should exist
    end
 end
 
 When /^within a couple of minutes the submission status will be updated$/ do
-  pending
+
 end
