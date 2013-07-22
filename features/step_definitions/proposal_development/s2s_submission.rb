06bc9529fdb27f137ff408aca55b89fc0eb7237d
Jonathan Utter <jutter@rsmart.com>
2013-07-22 10:31:41 -0700

    Developing scenarios that will successfully submit proposals through to s2s

--- features/step_definitions/proposal_development/s2s_submission.rb
+++ features/step_definitions/proposal_development/s2s_submission.rb
@@ -1,7 +1,10 @@
 Then /^Submission details will be immediately available on the S2S tab$/ do
-  pending
+   on S2S do |page|
+     page.expand_all
+     page.submission_details.should exist
+   end
 end
 
-When /^in 5 minutes the submission status will be updated$/ do
+When /^within a couple of minutes the submission status will be updated$/ do
   pending
 end
