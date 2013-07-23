3f197a8b2b921bdbd1baa140231580eb290ba5d5
Abraham Heward <aheward@rsmart.com>
2013-07-23 12:46:42 -0700

    Added basic page elements for unit administrator stuff.

--- lib/kuality-coeus/page_objects/maintenance.rb
+++ lib/kuality-coeus/page_objects/maintenance.rb
@@ -2,6 +2,6 @@ class Maintenance < BasePage
 
   page_url "#{$base_url}portal.do?selectedTab=portalMaintenanceBody"
 
-  links 'Institute Rate', 'Sponsor'
+  links 'Institute Rate', 'Sponsor', 'Unit Administrator'
 
 end
