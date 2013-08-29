17d1b318b5b36ef4b4c30d6643b22798e66de685
Abraham Heward <aheward@rsmart.com>
2013-08-29 12:10:10 -0700

    Started working on Group code again. Gem version bump.

--- lib/kuality-coeus/data_objects/identity/group.rb
+++ lib/kuality-coeus/data_objects/identity/group.rb
@@ -3,6 +3,7 @@ class GroupObject
   include Foundry
   include DataFactory
   include Navigation
+  include StringFactory
 
   attr_accessor :id, :namespace, :name, :description, :type,
                 :principal_name, :assignees
@@ -10,10 +11,31 @@ class GroupObject
   def initialize(browser, opts={})
     @browser = browser
 
+    defaults = {
+      type:        'Default',
+      namespace:   '::random::',
+      name:        random_alphanums,
+      description: random_alphanums,
+      assignees:   AssigneesCollection.new
+    }
+
+    set_options(defaults.merge(opts))
   end
 
   def create
 
   end
+
+  def add_assignee(opts={})
+    assignee = make AssigneeObject, opts
+    assignee.create
+    @assignees << assignee
+  end
+
+  # =========
+  private
+  # =========
+
+
 
 end
