553e3399000d413911c324d731ffa9a35e2648b4
Jonathan Utter <jutter@rsmart.com>
2013-09-18 15:11:40 -0700

    Removed unecessary elements from a few lookup pages

--- lib/kuality-coeus/page_objects/lookup_pages/award_lookup.rb
+++ lib/kuality-coeus/page_objects/lookup_pages/award_lookup.rb
@@ -1,12 +1,5 @@
 class AwardLookup < Lookups
 
   element(:award_id) { |b| b.frm.text_field(name: 'awardNumber') }
-  element(:sponsor_award_id) { |b| b.frm.text_field(name: 'sponsorAwardNumber') }
-  element(:account_id) { |b| b.frm.text_field(name: 'accountNumber') }
-  element(:award_status) { |b| b.frm.select(name: 'statusCode') }
-  element(:sponsor_id) { |b| b.frm.text_field(name: 'sponsorCode') }
-  element(:award_title) { |b| b.frm.text_field(name: 'title') }
-  element(:investigator) { |b| b.frm.text_field(name: 'projectPersons.fullName') }
-  element(:osp_administrator) { |b| b.frm.text_field(name: 'lookupOspAdministratorName') }
 
 end
--- lib/kuality-coeus/page_objects/lookup_pages/research_areas_lookup.rb
+++ lib/kuality-coeus/page_objects/lookup_pages/research_areas_lookup.rb
@@ -2,6 +2,4 @@ class ReasearchAreasLookup < Lookups
 
   element(:research_area_code) { |b| b.frm.text_field(name: 'researchAreaCode') }
   element(:parent_research_area_code) { |b| b.frm.text_field(name: 'parentResearchAreaCode') }
-
-  element(:research_area_code_description) { |b| b.frm.text_field(name: 'description') }
 end
