a41705d32214e4e858946132024e818d7fc65c9c
Abraham Heward <aheward@rsmart.com>
2013-10-04 12:46:35 -0700

    Built the Proposal Log Object and its create method.  Added another finder method to the User collection class.

--- lib/kuality-coeus/page_objects/central_admin.rb
+++ lib/kuality-coeus/page_objects/central_admin.rb
@@ -4,19 +4,4 @@ class CentralAdmin < BasePage
 
   green_buttons create_award: 'Award', create_proposal_log: 'Proposal Log'
 
-  # ============
-  private
-  # ============
-
-  # Use this to define methods to click on the green
-  # buttons on the page, all of which can be identified
-  # by the title tag. The method takes a hash, where the key
-  # will become the method name, and the value is the string
-  # that matches the green button's link title tag.
-  def green_buttons(links={})
-    links.each_pair do |name, title|
-      action(name) { |b| b.frm.link(title: title).click; b.loading }
-    end
-  end
-
 end
