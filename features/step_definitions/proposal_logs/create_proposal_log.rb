f8430b48cbb4dcb07848f76fd720673b9b865eb3
Abraham Heward <aheward@rsmart.com>
2013-11-14 12:36:03 -0700

    Stylistic changes in step definitions.

--- features/step_definitions/proposal_logs/create_proposal_log.rb
+++ features/step_definitions/proposal_logs/create_proposal_log.rb
@@ -13,7 +13,7 @@ When /^I? ?initiate a Proposal Log$/ do
   @proposal_log = create ProposalLogObject
 end
 
-Then(/^the proposal log type of the proposal log document should be (.*)$/) do |status|
+Then(/^the proposal log type of the Proposal Log should be (.*)$/) do |status|
   @proposal_log2.proposal_log_type.should == status
 end
 
@@ -25,13 +25,13 @@ When /^the Proposal Log status should be (.*)$/ do |prop_log_status|
   @proposal_log.log_status.should == prop_log_status
 end
 
-When /^I submit a new permanent proposal log document with the same PI into routing$/ do
+When /^I submit a new permanent Proposal Log with the same PI into routing$/ do
   @proposal_log2 = create ProposalLogObject,
                           principal_investigator: @temp_proposal_log.principal_investigator
   @proposal_log2.submit
 end
 
-When(/^I initiate a permanent Proposal Log$/) do
+When /^I initiate a permanent Proposal Log$/ do
   @proposal_log = create ProposalLogObject
 end
 
@@ -49,18 +49,18 @@ Then /^I merge my new proposal log with my previous temporary proposal log$/ do
   end
 end
 
-When /^I submit a new proposal log$/ do
+When /^I submit a new Proposal Log$/ do
   @proposal_log = create ProposalLogObject
   @proposal_log.submit
 end
 
-When /^I submit a new temporary proposal log document$/ do
+When /^I submit a new Temporary Proposal Log$/ do
   @temp_proposal_log = create ProposalLogObject,
                               log_type: 'Temporary'
   @temp_proposal_log.submit
 end
 
-Then /^the proposal log's status should reflect it has been merged$/ do
+Then /^the Proposal Log's status should reflect it has been merged$/ do
   on(Researcher).search_proposal_log
   on
 end
