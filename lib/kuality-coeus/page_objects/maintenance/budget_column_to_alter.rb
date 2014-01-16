9531002aaab5cfae439d01bb42285037f9c23906
Abraham Heward <aheward@rsmart.com>
2014-01-16 13:01:14 -0700

    Removal of useless features. Removal of @failing tag from a couple scenarios. Added some nokogiri methods for large select lists.

--- lib/kuality-coeus/page_objects/maintenance/budget_column_to_alter.rb
+++ lib/kuality-coeus/page_objects/maintenance/budget_column_to_alter.rb
@@ -9,4 +9,9 @@ class BudgetColumnToAlter < BasePage
   element(:lookup_argument) { |b| b.frm.select(name: 'document.newMaintainableObject.lookupClass') }
   element(:lookup_return) { |b| b.frm.select(name: 'document.newMaintainableObject.lookupReturn') }
 
+  # Because the Lookup Argument select list is 450 items long, we have this
+  # Nokogiri code here, which returns the list much faster than Watir does.
+  # This enables, for example, a faster randomized item selection from the list...
+  value(:lookup_argument_list) { |b| b.noko.select(name: 'document.newMaintainableObject.lookupClass').options.map {|opt| opt.text }[1..-1] }
+
 end
