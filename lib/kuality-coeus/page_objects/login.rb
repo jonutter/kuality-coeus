7b1c4121e31a016c0b3dc13075329c74183f3f49
Abraham Heward <aheward@rsmart.com>
2014-01-30 11:17:53 -0700

    Back to 5.1.1 stuff, for now...

--- lib/kuality-coeus/page_objects/login.rb
+++ lib/kuality-coeus/page_objects/login.rb
@@ -3,6 +3,6 @@ class Login < BasePage
   page_url "#{$base_url}backdoorlogin.do"
 
   element(:username) { |b| b.text_field(name: /login_user/) }
-  action(:login) { |b| b.button(id: 'Rice-LoginButton').click; b.loading }
+  button 'Login'
 
 end
