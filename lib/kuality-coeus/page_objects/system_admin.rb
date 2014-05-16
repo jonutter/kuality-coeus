4f257028777e3ba4e12df3bbcf3f0b87fe95e0ae
Abraham Heward <aheward@rsmart.com>
2014-05-16 11:18:31 -0700

    CAS Login stuff.

--- lib/kuality-coeus/page_objects/system_admin.rb
+++ lib/kuality-coeus/page_objects/system_admin.rb
@@ -1,6 +1,6 @@
 class SystemAdmin < BasePage
 
-  page_url "#{$base_url}portal.do?selectedTab=portalSystemAdminBody"
+  page_url "#{$base_url+$context}portal.do?selectedTab=portalSystemAdminBody"
 
   links 'Person', 'Person Extended Attributes', 'Group', 'Role', 'Parameter'
 
